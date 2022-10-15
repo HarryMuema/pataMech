f0289f23b0e99a3cf1470cd70438ab5fa95cf4d4
Harry Muema <harymueema@gmail.com>
2022-10-15 14:12:49 +0300

    PAT-9:validation point

--- pataMech-node-api/server/validation/signUpvalidation.js
+++ pataMech-node-api/server/validation/signUpvalidation.js
@@ -3,10 +3,39 @@ const joi=require('joi')
 module.exports={
     signUpValidation:(user)=>{
         const schema=joi.object({
-            firstname:joi.string().alphanum().min(3).max(30).required(),
-            lastname:joi.string().alphanum().min(3).max(30).required(),
             email:joi.string().email().required(),
             password:joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
         })
+        return schema.validate(user)
     },
+    signUpDetails1Validation:(data)=>{
+        const schema=joi.object({
+            firstname:joi.string().alphanum().min(3).max(30).required(),
+            lastname:joi.string().alphanum().min(3).max(30).required(),
+            phonenumber:joi.number().integer().min(9).max(9).required(),
+        })
+        return schema.validate(data)
+    },
+    signUpDetails2Validation:(data)=>{
+        const schema=joi.object({
+            address1:joi.string().min(3),
+            address2:joi.string().min(3),
+            carmodel:joi.string().min(4),
+            carmake:joi.string().min(3),
+            carplatenumber:joi.string().min(7).max(7),
+            carmodelyear:joi.number().integer().min(4).max(4),
+        })
+        return schema.validate(data)
+    },
+    signUpDetails2Validationcenters:(data)=>{
+        const schema=joi.object({
+            address1:joi.string().min(3),
+            address2:joi.string().min(3),
+            centername:joi.string().required(),
+            town:joi.string().required(),
+            county:joi.string().required(),
+            postalcode:joi.string().min(3),
+            telephone:joi.number().integer(),
+        })
+    }
 }
